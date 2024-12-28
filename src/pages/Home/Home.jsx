52986058a922c0e115fb6eee982c9f621eeed1a6
kasun wilbagedara <kasunwilbagedara2001@gmail.com>
2024-12-28 20:02:44 +0530

    food display

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './Home.css'
 import Header from '../../componments/Header/Header'
 import ExploreMenu from '../../componments/ExploreMenu/ExploreMenu'
-import {useState} from 'react'
+import FoodDisplay from '../../componments/FoodDisplay/FoodDisplay'
 
+import {useState} from 'react'
 const Home = () => {
 
   const[category,setCategory]=useState("All");
@@ -12,8 +13,9 @@ const Home = () => {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
 
-export default Home
+export default Home;
